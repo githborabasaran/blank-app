7550f964e4d26986181442e125a319603e654ac4
Bora Başaran <101041033+githborabasaran@users.noreply.github.com>
2025-06-07 00:06:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
 from sklearn.feature_selection import RFE
 import os
-
 from imblearn.under_sampling import RandomUnderSampler  # Import RandomUnderSampler
 import joblib
 from sklearn.metrics import accuracy_score, classification_report, roc_auc_score, confusion_matrix, roc_curve, auc
